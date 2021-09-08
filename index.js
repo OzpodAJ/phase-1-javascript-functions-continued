ca3f0815bb4ad4ec6f5a054247e410c56d7d8d49
Andrew Osborn <osbornandrewj@gmail.com>
2021-09-08 13:10:32 -0700

    commit tree 2eb93dc9bd2f4a7af9ecd18911040bb69279bb39

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate'){
+   return (`This Saturday, I want to ${activity}!`);
+}
+function mondayWork(task = 'go to the office'){
+   return (`This Monday, I will ${task}.`)
+}
+function wrapAdjective(a = '*'){
+   return function inner(b = `a dedicated programmer`){
+      return `You are ${a}${b}${a}!`
+   }
+}
